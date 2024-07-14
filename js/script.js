a11a91aa886c60806f421240f47499c3248fc838
IgorRudenko108 <88731084+IgorRudenko108@users.noreply.github.com>
2024-07-14 13:50:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+// -- copy-link ------------------------------------------------------------------------------
+const copyBtn = document.querySelector('.earn-block__btn');
+const copyBtnText = document.querySelector('.earn-block__btn-text');
+const linkToCopy = document.querySelector('.earn-block__link');
 
-
-
+copyBtn.addEventListener('click', () => {
+  const copyText = linkToCopy.innerText;
+  if (linkToCopy) {
+    navigator.clipboard.writeText(copyText)
+      .then(() => {
+         copyText.innerText = 'Link copied';
+        if (copyBtn.innerText !== 'Link copied') {
+         copyBtnText.innerText = 'Link copied';
+         copyBtn.classList.add('copied');
+         }
+      })
+      .catch(err => {
+        console.log('Something went wrong', err);
+      })
+  }
+});
+// -- /copy-link ------------------------------------------------------------------------------
